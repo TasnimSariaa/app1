89a896d958e278583861402b8d69a39ad39eea63
saria <tasnim.saria33@gmail.com>
2023-09-11 21:26:01 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/organizationSari/app1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.organizationSari.app1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'dart:js';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: true, home: HomeActivity());
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.account_circle,
+                color: Colors.green,
+                size: 65,
+              ),
+              Text("Jhon Doe",
+                  style: TextStyle(fontSize: 40, color: Colors.green)),
+              Text("Flutter Batch 4",
+                  style: TextStyle(fontSize: 30, color: Colors.blue))
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: Text("Profile"),
+          //titleSpacing: 90,
+          centerTitle: true,
+          toolbarHeight: 60,
+          elevation: 10,
+          toolbarOpacity: 1,
+          backgroundColor: Colors.blue,
+        ));
+  }
+}
