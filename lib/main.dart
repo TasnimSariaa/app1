1ffbb3eddefc24556d927d6c80f7f83d13462bae
TasnimSariaa <141612798+TasnimSariaa@users.noreply.github.com>
2023-11-03 23:50:36 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,104 @@
-import 'dart:js';
-
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(const MyApp());
+void main() {
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Weather {
+  final String city;
+  final int temperature;
+  final String condition;
+  final int humidity;
+  final double windSpeed;
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: true, home: HomeActivity());
+  Weather({
+    required this.city,
+    required this.temperature,
+    required this.condition,
+    required this.humidity,
+    required this.windSpeed,
+  });
+
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    return Weather(
+      city: json['city'],
+      temperature: json['temperature'],
+      condition: json['condition'],
+      humidity: json['humidity'],
+      windSpeed: json['windSpeed'].toDouble(),
+    );
   }
 }
 
-class HomeActivity extends StatelessWidget {
-  const HomeActivity({super.key});
+class MyApp extends StatelessWidget {
+  final List<Weather> weatherData = [
+    Weather(
+      city: "New York",
+      temperature: 20,
+      condition: "Clear",
+      humidity: 60,
+      windSpeed: 5.5,
+    ),
+    Weather(
+      city: "Los Angeles",
+      temperature: 25,
+      condition: "Sunny",
+      humidity: 50,
+      windSpeed: 6.8,
+    ),
+    Weather(
+      city: "London",
+      temperature: 15,
+      condition: "Partly Cloudy",
+      humidity: 70,
+      windSpeed: 4.2,
+    ),
+    Weather(
+      city: "Tokyo",
+      temperature: 28,
+      condition: "Rainy",
+      humidity: 75,
+      windSpeed: 8.0,
+    ),
+    Weather(
+      city: "Sydney",
+      temperature: 22,
+      condition: "Cloudy",
+      humidity: 55,
+      windSpeed: 7.3,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.account_circle,
-                color: Colors.green,
-                size: 65,
-              ),
-              Text("Jhon Doe",
-                  style: TextStyle(fontSize: 40, color: Colors.green)),
-              Text("Flutter Batch 4",
-                  style: TextStyle(fontSize: 30, color: Colors.blue))
-            ],
-          ),
-        ),
+    return MaterialApp(
+      home: Scaffold(
         appBar: AppBar(
-          title: Text("Profile"),
-          //titleSpacing: 90,
-          centerTitle: true,
-          toolbarHeight: 60,
-          elevation: 10,
-          toolbarOpacity: 1,
-          backgroundColor: Colors.blue,
-        ));
+          title: Text('Weather Information'),
+        ),
+        body: ListView.builder(
+          itemCount: weatherData.length,
+          itemBuilder: (context, index) {
+            Weather weather = weatherData[index];
+            return Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('City: ${weather.city}'),
+                    Text('Temperature: ${weather.temperature}°C'),
+                    Text('Condition: ${weather.condition}'),
+                    Text('Humidity: ${weather.humidity}%'),
+                    Text('Wind Speed: ${weather.windSpeed} m/s'),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
